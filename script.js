1800eb9f866b5324e57b554cd91effda3db94993
Casey Jones <jcaseyj.5@gmail.com>
2020-02-04 15:34:44 -0700

    r is now also properly animated

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ window.onscroll = function() {
         skillsBox = document.getElementById('myFace'),
         linFill = document.getElementById('lin_fill'),
         linUnfill = document.getElementById('lin_unfill');
-
+        rFill = document.getElementById('r_fill'),
+        rUnfill = document.getElementById('r_unfill');
     this.console.log(skillsBox.offsetTop)
 
     if (window.pageYOffset >= skillsBox.offsetTop) {
         this.console.log("reached");
         linFill.className = 'linux_fill_change';
         linUnfill.className = 'linux_unfill_change';
+        rFill.className = 'r_fill_change';
+        rUnfill.className = 'r_unfill_change';
 
     }
     if (scrolled => imgLeft.height) {}
